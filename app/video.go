8f01add3134394a766fa8650cc652c4f3ec38295
Leonardo Severo <leopedroso45@gmail.com>
2020-07-05 17:08:44 -0700

    Some changes...

--- app/video.go
+++ app/video.go
@@ -136,7 +136,7 @@ func PostVideo(w http.ResponseWriter, r *http.Request) {
 	}
 	tempFile.Write(fileBytes)
 
-	staticPath := "http://192.168.43.216:8000"
+	staticPath := "http://192.168.1.104:8000"
 	currentPath := strings.Replace(tempFile.Name(), "data/", staticPath+"/data/", -1)
 
 	videoA := VideoConstructor(handler.Filename, handler.Filename, currentPath)
