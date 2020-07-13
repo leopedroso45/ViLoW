1c318030d60010b689cc63744e9cdf68badd822f
Leonardo Severo <leopedroso45@gmail.com>
2020-07-13 01:52:54 -0700

    Quick Update

--- oauth/googleOAuth2.go
+++ oauth/googleOAuth2.go
@@ -19,7 +19,7 @@ func init() {
 	GoogleOauthConfig = &oauth2.Config{
 		RedirectURL:  "http://localhost:8000/callback",
 		ClientID:     "123149391338-nnv2p8jvq8v7ed8jkqldp9gqr2t5lic3.apps.googleusercontent.com",
-		ClientSecret: "9ECnkJbHwrHdMuEh0lJkASlQ",
+		ClientSecret: "",
 		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email"},
 		Endpoint:     google.Endpoint,
 	}
